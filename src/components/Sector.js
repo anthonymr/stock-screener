6c10b0d61734222237ed3f3fd62a85725c0aa89e
Anthony Martin <anthonymartin20@gmail.com>
2023-03-13 19:58:14 +0100

    Add sectors style

--- src/components/Sector.js
+++ src/components/Sector.js
@@ -1,12 +1,48 @@
 import { NavLink } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faArrowsSpin,
+  faBellConcierge,
+  faBolt,
+  faBuilding,
+  faCoins,
+  faEnvelope,
+  faGem,
+  faIndustry,
+  faMicrochip,
+  faNotesMedical,
+  faShield,
+  faArrowRight,
+} from '@fortawesome/free-solid-svg-icons';
+import style from '../styles/Sector.module.css';
 
-function Sector({ sector, change }) {
+function Sector({ sector, change, className }) {
+  const icons = {
+    'Basic Materials': faGem,
+    'Communication Services': faEnvelope,
+    'Consumer Cyclical': faArrowsSpin,
+    'Consumer Defensive': faShield,
+    Energy: faBolt,
+    'Financial Services': faCoins,
+    Healthcare: faNotesMedical,
+    Industrials: faIndustry,
+    'Real Estate': faBuilding,
+    Technology: faMicrochip,
+    Utilities: faBellConcierge,
+  };
+
   const url = encodeURI(`sectors/${sector}`);
   return (
-    <NavLink to={url}>
-      <h1>{sector}</h1>
-      <span>{change}</span>
+    <NavLink to={url} className={`${style.sector} ${className}`}>
+      <div className={style.iconContainer}>
+        <FontAwesomeIcon icon={icons[sector]} />
+        <FontAwesomeIcon icon={faArrowRight} />
+      </div>
+      <div className={style.infoContainer}>
+        <h3>{sector}</h3>
+        <span>{change}</span>
+      </div>
     </NavLink>
   );
 }
@@ -14,6 +50,7 @@ function Sector({ sector, change }) {
 Sector.propTypes = {
   sector: PropTypes.string.isRequired,
   change: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
 };
 
 export default Sector;
--- src/components/Sectors.js
+++ src/components/Sectors.js
@@ -1,7 +1,11 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 import { fetchSectors } from '../redux/sectors/sectorsSlice';
 import Sector from './Sector';
+import stocks from '../assets/images/stocks2.jpg';
+import style from '../styles/Sectors.module.css';
 
 function Sectors() {
   const { sectors } = useSelector((state) => state.sectors);
@@ -11,18 +15,52 @@ function Sectors() {
     dispatch(fetchSectors());
   }, [dispatch]);
 
+  let className = 'is-light';
+  let isLight = true;
+
   return (
-    <>
-      {
-        sectors.map((sector) => (
-          <Sector
-            key={sector.sector}
-            sector={sector.sector}
-            change={sector.changesPercentage}
-          />
-        ))
-      }
-    </>
+    <section>
+      <header>
+        <div />
+        <div>Stock Screaner</div>
+        <div><FontAwesomeIcon icon={faGear} /></div>
+      </header>
+      <div className="picture-container">
+        <img src={stocks} alt="stock market" />
+      </div>
+      <h2>Economical Sectors</h2>
+      <div className={style.sectorsWrapper}>
+        <section className={style.sectorsContainer}>
+          {
+            sectors.map((sector, index) => {
+              if (index === 0) {
+                isLight = false;
+              } else if (index % 2 !== 0) {
+                if (!isLight) className = 'is-dark';
+                else if (isLight) className = 'is-light';
+              } else if (index % 2 === 0) {
+                if (!isLight) {
+                  className = 'is-dark';
+                  isLight = true;
+                } else if (isLight) {
+                  className = 'is-light';
+                  isLight = false;
+                }
+              }
+
+              return (
+                <Sector
+                  key={sector.sector}
+                  sector={sector.sector}
+                  change={sector.changesPercentage}
+                  className={className}
+                />
+              );
+            })
+          }
+        </section>
+      </div>
+    </section>
   );
 }
 
