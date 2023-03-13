8c6e42da9c92e625b67f337c1b7b77e8b7a16f30
Anthony Martin <anthonymartin20@gmail.com>
2023-03-13 16:12:12 +0100

    Add key file

---
+++ src/key.js
@@ -0,0 +1,5 @@
+export const URL = 'https://financialmodelingprep.com/api/v3/';
+export const SECTORSENDPOINT = 'sectors-performance';
+export const COMPANIESENDPOINT = 'stock-screener';
+export const COMPANYENDPOINT = 'profile/';
+export const APIKEY = '';
